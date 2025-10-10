f840900cdddfbf405e783c301e6d9d9adb59640e
vemolista <tomas@tomasvemola.com>
2025-10-10 13:02:24 +0200

    add logical clock

---
+++ common/clock.go
@@ -0,0 +1,14 @@
+package common
+
+// See https://martinfowler.com/articles/patterns-of-distributed-systems/lamport-clock.html
+
+type LamportClock struct {
+	timestamp int64
+}
+
+func (c *LamportClock) Tick(requestTime int64) int64 {
+	latestTime := max(c.timestamp, requestTime)
+	latestTime += 1
+
+	return latestTime
+}
