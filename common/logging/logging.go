25b0093a51a8d8ad2b2c99d085aac9749eed44c7
vemolista <tomas@tomasvemola.com>
2025-10-24 11:37:25 +0200

    add event

--- common/logging/logging.go
+++ common/logging/logging.go
@@ -20,19 +20,17 @@ type Client struct {
 func (Server) component() {}
 func (Client) component() {}
 
-func format(c Component, message string, timestamp int32) string {
+func format(c Component, event string, message string, timestamp int32) string {
 	switch v := c.(type) {
 	case Server:
-		return fmt.Sprintf("[server] (logical timestamp: %d) (message: %s)\n", timestamp, message)
+		return fmt.Sprintf("[server] (event: %s) (logical timestamp: %d) (message: %s)\n", event, timestamp, message)
 	case Client:
-		return fmt.Sprintf("[client '%s'] (logical timestamp: %d) (message: %s)\n", v.username, timestamp, message)
+		return fmt.Sprintf("[client '%s'] (event: %s) (logical timestamp: %d) (message: %s)\n", v.username, event, timestamp, message)
 	default:
-		return fmt.Sprintf("[unknown] (logical timestamp: %d) (message: %s)\n", timestamp, message)
+		return fmt.Sprintf("[unknown] (event: %s) (logical timestamp: %d) (message: %s)\n", event, timestamp, message)
 	}
 }
 
-func Log(c Component, message string, timestamp int32) {
-	logMsg := format(c, message, timestamp)
-
-	log.Printf("%s", logMsg)
+func Log(c Component, event, message string, timestamp int32) {
+	log.Printf("%s", format(c, event, message, timestamp))
 }
